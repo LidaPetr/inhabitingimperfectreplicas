ff74af2ac474e1617534a25715a1c0936d734eaf
LidaPetr <lpetropoulou@homex.com>
2021-04-24 05:07:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function navSlide() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
